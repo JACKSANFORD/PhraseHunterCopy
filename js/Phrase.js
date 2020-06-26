f51d39ca0ded8ab8f129f1af1310c2bd7965d4d1
Olivia Park <olivia.park1346@gmail.com>
2020-06-26 20:14:14 +0900

    Added comments to Phrase

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,15 +7,16 @@
  	constructor(phrase) {
 
  	}
- 	
+ 	//add letter placeholders 
+ 	//on li element for each letter 
  	addPhraseToDisplay() {
 
  	}
-
+ 	//checks to see if the letters match 
  	checkLetter() {
 
  	}
-
+ 	//reveals letter on the board that matches the player's selction 
  	showMatchedLetter() {
  		
  	}
