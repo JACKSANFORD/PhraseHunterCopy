534ffa5afeb6a14892bb4c884fa5aa9be1a453e7
Olivia Park <olivia.park1346@gmail.com>
2020-06-26 20:07:44 +0900

    Adding classes to phrase and game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ //Game Class 
+ class Game {
+
+ 	constructor() {
+ 	
+ 	}
+
+ 	startGame() {
+
+ 	}
+
+ 	getRandomPhrase() {
+
+ 	}
+
+ 	handleInteraction() {
+
+ 	}
+
+ 	removeLife() {
+
+ 	}
+
+ 	checkForWin() {
+
+ 	}
+
+ 	gameOver() {
+
+ 	}
+
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,22 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+//Phrase Class 
+ class Phrase {
+ 	constructor(phrase) {
+
+ 	}
+ 	
+ 	addPhraseToDisplay() {
+
+ 	}
+
+ 	checkLetter() {
+
+ 	}
+
+ 	showMatchedLetter() {
+ 		
+ 	}
+ }
